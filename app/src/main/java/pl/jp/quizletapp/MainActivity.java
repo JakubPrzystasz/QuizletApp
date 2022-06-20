e67776c7f046840d4599a8e1558a0d96326eb77b
Jakub Przystasz <jakub.przystasz@gmail.com>
2022-06-20 15:33:06 +0200

    Initial commit

---
+++ app/src/main/java/pl/jp/quizletapp/MainActivity.java
@@ -0,0 +1,14 @@
+package pl.jp.quizletapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
