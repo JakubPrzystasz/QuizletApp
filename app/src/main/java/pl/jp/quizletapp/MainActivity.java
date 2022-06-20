6da3aee9a36055773b3dc0d59ecb9afe24533e0c
Jakub Przystasz <jakub.przystasz@gmail.com>
2022-06-20 18:31:28 +0200

    Add login screen

--- app/src/main/java/pl/jp/quizletapp/MainActivity.java
+++ app/src/main/java/pl/jp/quizletapp/MainActivity.java
@@ -1,14 +1,82 @@
 package pl.jp.quizletapp;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatEditText;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.webkit.URLUtil;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    Intent data = result.getData();
+                    AppCompatEditText et_server = (AppCompatEditText) findViewById(R.id.et_server);
+                    et_server.setText(Objects.requireNonNull(data).getStringExtra("server"));
+                }
+            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        checkPermission(Manifest.permission.INTERNET,100);
         setContentView(R.layout.activity_main);
+
+        final Button btn_scan = (Button) findViewById(R.id.btn_scan);
+        final Button btn_start = (Button) findViewById(R.id.btn_start);
+
+        btn_scan.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, ScanActivity.class);
+            someActivityResultLauncher.launch(intent);
+        });
+
+        btn_start.setOnClickListener(v -> {
+            final AppCompatEditText et_server = (AppCompatEditText) findViewById(R.id.et_server);
+            final AppCompatEditText et_login = (AppCompatEditText) findViewById(R.id.et_login);
+
+            et_login.setText("j0tp3");
+            et_server.setText("http://192.168.1.102:7070/java");
+
+            if(!URLUtil.isValidUrl(String.valueOf(et_server.getText()))){
+                Toast.makeText(this, "Adres serwera jest nieprawidłowy", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            if(TextUtils.isEmpty(et_login.getText())){
+                Toast.makeText(this, "Login jest nieprawidłowy", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            //use retrofit to check connection
+
+            Toast.makeText(this, "Logowanie...", Toast.LENGTH_SHORT).show();
+
+        });
     }
+
+    public void checkPermission(String permission, int requestCode)
+    {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, permission) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[] { permission }, requestCode);
+        }
+    }
+
 }
---
+++ app/src/main/java/pl/jp/quizletapp/ScanActivity.java
@@ -0,0 +1,90 @@
+package pl.jp.quizletapp;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.URLUtil;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.budiyev.android.codescanner.ScanMode;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.Result;
+
+import java.util.List;
+
+public class ScanActivity extends AppCompatActivity {
+    private CodeScanner mCodeScanner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan);
+
+        checkPermission(Manifest.permission.CAMERA,100);
+
+        CodeScannerView scannerView = findViewById(R.id.scanner_view);
+        mCodeScanner = new CodeScanner(this, scannerView);
+        mCodeScanner.setCamera(CodeScanner.CAMERA_BACK);
+        mCodeScanner.setAutoFocusEnabled(true);
+        mCodeScanner.setFormats(List.of(BarcodeFormat.QR_CODE));
+        mCodeScanner.setScanMode(ScanMode.CONTINUOUS);
+
+        mCodeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull final Result result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if(URLUtil.isValidUrl(result.getText())){
+                            Intent intent = new Intent();
+                            intent.putExtra("server", result.getText());
+
+                            setResult(Activity.RESULT_OK, intent);
+                            finish();
+                        }else{
+                            Toast.makeText(ScanActivity.this, "Podany kod jest nieprawidłowy", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        scannerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCodeScanner.startPreview();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mCodeScanner.startPreview();
+    }
+
+    @Override
+    protected void onPause() {
+        mCodeScanner.releaseResources();
+        super.onPause();
+    }
+
+    public void checkPermission(String permission, int requestCode)
+    {
+        // Checking if permission is not granted
+        if (ContextCompat.checkSelfPermission(ScanActivity.this, permission) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(ScanActivity.this, new String[] { permission }, requestCode);
+        }
+    }
+}
